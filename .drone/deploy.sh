67613663c030faf551cbd1dd9834dbce3c70f43b
liquuid <liquuid@hacklab.com.br>
2015-09-26 23:43:38 -0300

    retirando verbose do rsync

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avp src/* core@povosisolados.hacklab.com.br:/srv/www
-chmod -R o+w src/wp-content/uploads
+rsync -azp src/* core@povosisolados.hacklab.com.br:/srv/www
+chmod -R -v og+w src/wp-content/uploads
