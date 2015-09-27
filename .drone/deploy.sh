6f585207aac7dd46c78ec575903a4e9915e187c0
liquuid <liquuid@hacklab.com.br>
2015-09-27 00:00:06 -0300

    teste

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -azp src/* core@povosisolados.hacklab.com.br:/srv/www
-ssh core@povosisolados.hacklab.com.br chmod -R og+w /srv/www/wp-content/uploads
+ssh core@povosisolados.hacklab.com.br chmod -R og+w /srv/www/wp-content/uploads || echo ok
