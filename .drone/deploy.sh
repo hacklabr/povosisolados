bc689656a9b4a82a99d550620849c4274eafe95b
Fernando Silva <liquuid@liquuid.net>
2015-10-05 12:04:56 -0300

    melhorando script de deploy

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo 'fazendo rsync dos arquivos'
 rsync -avzp src/* core@povosisolados.hacklab.com.br:/srv/www
+echo 'acertando as permissões' 
 ssh core@povosisolados.hacklab.com.br chmod -Rv og+w /srv/www/wp-content/uploads || echo ok
