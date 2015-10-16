883a02fb8be7289903b54ace3fcf0f34ecfc1002
Fernando Silva <liquuid@hacklab.com.br>
2015-10-16 15:28:41 -0300

    pega featured image da page

--- src/wp-content/themes/Quark/header-boletim.php
+++ src/wp-content/themes/Quark/header-boletim.php
@@ -21,19 +21,19 @@ global $post;
 	<?php if(get_theme_mod('quark_cookie_enable', 1) == 1) : ?>
 		<?php get_template_part('cookielaw'); ?>
 	<?php endif; ?>
-	
+
 	<?php do_action('quark_head'); ?>
 	<?php wp_head(); ?>
 </head>
 
-<?php 
+<?php
 	$front_class = 'boletim';
-	if (get_theme_mod('quark_dark_image_frontpage', 0) == 1) { 
-		$front_class .= ' dark-bg'; 
-	} 
+	if (get_theme_mod('quark_dark_image_frontpage', 0) == 1) {
+		$front_class .= ' dark-bg';
+	}
 
-	if (get_theme_mod('quark_js_parallax', 1) == 1) { 
-		$front_class .= ' js-parallax'; 
+	if (get_theme_mod('quark_js_parallax', 1) == 1) {
+		$front_class .= ' js-parallax';
 	}
 
 	$logo_image = get_theme_mod('quark_logo', '');
@@ -44,7 +44,7 @@ global $post;
 	<?php if (get_theme_mod('quark_page_loader', 1) == 1) : ?>
 		<div id="gk-page-preloader"></div>
 	<?php endif; ?>
-	
+
 	<!--[if lte IE 8]>
 	<div id="ie-toolbar"><div><?php _e('You\'re using an unsupported version of Internet Explorer. Please <a href="http://windows.microsoft.com/en-us/internet-explorer/products/ie/home">upgrade your browser</a> for the best user experience on our site. Thank you.', 'quark') ?></div></div>
 	<![endif]-->
@@ -56,17 +56,17 @@ global $post;
 			<div id="gk-header-nav" <?php if((get_theme_mod('quark_menu_fixed', 1) == 1 && get_theme_mod('quark_menu_classic', 0) == 1)) { echo 'class="gk-fixed"'; } ?>>
 				<div>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'quark' ); ?>"><?php _e( 'Skip to content', 'quark' ); ?></a>
- 
+
 	                <div id="gk-mobile-menu">
                      	<span id="gk-mobile-menu-text"><?php _e('Boletins','quark'); ?></span>
                     	<i id="static-aside-menu-toggler"></i>
 	                </div>
-                 	
+
                  </div>
 			</div>
 
 			<div id="gk-header-mod">
-				<img src="<?php header_image(); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+				<?php the_post_thumbnail('full'); ?>
 				<div class="frontpage-block-wrap">
 					<?php do_action('quark_before_header'); ?>
 
@@ -75,7 +75,7 @@ global $post;
                         <?php while ( have_posts() ) : the_post(); ?>
 							<?php the_content(); ?>
 						<?php endwhile; ?>
-						
+
 						<?php wp_reset_query(); ?>
 
 						<?php if(get_theme_mod('quark_header_mouse_icon',1) == 1) : ?>
