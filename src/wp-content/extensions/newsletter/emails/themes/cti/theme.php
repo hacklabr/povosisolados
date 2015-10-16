a8396fd98a8889922c960a9ee4b3acdef752e065
Ariadne Melo <ariadnevmelo@gmail.com>
2015-10-16 10:50:04 -0300

    fixing the newsletter layout

--- src/wp-content/extensions/newsletter/emails/themes/cti/theme.php
+++ src/wp-content/extensions/newsletter/emails/themes/cti/theme.php
@@ -40,10 +40,8 @@ if (isset($theme_options['theme_posts'])) {
         </style>
     </head>
     <body style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 14px; color: #fff; margin: 0 auto; padding: 0;">
-    
-    <p style="text-align: center; font-size: small;"><a target="_blank" href="{email_url}">View this email online</a></p>
 
-    <table cellspacing="0" cellpadding="0" width="600" style="margin: 0 auto;">
+    <table bgcolor="#CCAE6D" cellspacing="0" cellpadding="0" width="600" style="margin: 0 auto;">
       <!-- top header -->
       <tr bgcolor="#9F2032">
         <td align="left" bgcolor="#9F2032" colspan="2" style="color: #fff; font-size: 20px; font-weight: bold; padding: 15px 10px;">Boletim trimestral</td>
@@ -80,24 +78,29 @@ if (isset($theme_options['theme_posts'])) {
             <table>
                 <tr>
                 <?php foreach ($posts as $post) { setup_postdata($post); ?>
-                    <td width="270" valign="top" style="padding: 18px 0px 18px 18px;">
-                        <a target="_blank" href="<?php echo get_permalink($post); ?>">
-                        <img height="200" width="270" src="<?php echo newsletter_get_post_image($post->ID); ?>"></a>
+                    <td width="270" valign="top" style="padding: 18px 0px 0px 18px;">
+                    <a target="_blank" href="#" href="<?php echo get_permalink($post); ?>">
+                        <img height="150" width="270" src="<?php echo newsletter_get_post_image($post->ID); ?>"></a>
                         <div style="background-color: #230e0f; color: #fff; font-size: 14px; font-weight: bold; margin-top: -3px; padding: 10px 15px;">
-                            <a target="_blank" href="<?php echo get_permalink(); ?>" style="color: #fff;">
-                            <?php the_title(); ?></a>
+                          <a target="_blank" href="<?php echo get_permalink(); ?>" style="color: #fff;">
+                              <?php the_title(); ?>
+                          </a>
                         </div> 
                     </td>
                 <?php } ?>
                 </tr>
-            </table>            
+            <</table>            
         </td>
       </tr>
       <tr>
-        <td align="center" bgcolor="#9F2032" colspan="3" style="padding: 10px;">
-          <a href="#" style="color: #fff;">Facebook</a>
-          <a href="#" style="color: #fff;">Twitter</a>
-          <a href="#" style="color: #fff;">Youtube</a>
+        <!-- blank space -->
+        <td bgcolor="#CCAE6D" colspan="3" height="18"></td>
+      </tr>
+      <tr>
+        <td align="center" bgcolor="#9F2032" colspan="3" style="padding: 10px;" valign="top">
+          <a href="#" style="color: #fff;"><img src="<?php echo $theme_url ?>/facebook.png" height="25"></a>
+          <a href="#" style="color: #fff; margin: 0 10px 0 5px"><img src="<?php echo $theme_url ?>/twitter.png"></a>
+          <a href="#" style="color: #fff;"><img src="<?php echo $theme_url ?>/youtube.png"></a>
         </td>
       </tr>
       <tr bgcolor="#CCAE6D">
@@ -115,6 +118,9 @@ if (isset($theme_options['theme_posts'])) {
         </td>
       </tr>
     </table>
+
+
+    <p style="text-align: center; font-size: small;"><a target="_blank" href="{email_url}">View this email online</a></p>
 
   </body>
 </html>
