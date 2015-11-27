a073188035f4c7b8c271f4c86c4603b27dc3ba6c
Fernando Silva <liquuid@liquuid.net>
2015-11-27 00:55:30 -0200

    checkbox post editorial

---
+++ src/wp-content/themes/Quark-boletim/piklist/parts/meta-boxes/checkbox-post-editorial.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Title: Post Editorial
+Post Type: post
+*/
+piklist('field', array(
+'type' => 'checkbox'
+,'field' => 'is_editorial'
+,'label' => 'Editorial'
+,'description' => 'Selecione a opção ao lado caso esse post seja um editorial'
+,'attributes' => array(
+  'class' => 'text'
+)
+,'choices' => array(
+  'boletim' => 'É um editorial'
+)
+));
+?>
