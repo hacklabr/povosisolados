5060db47223399ab13f14aa999e656d565297dc6
Fernando Silva <liquuid@liquuid.net>
2015-12-11 00:33:34 -0200

    testando tamanho de thumb para destaque boletim

--- src/wp-content/themes/Quark-boletim/template.boletim.php
+++ src/wp-content/themes/Quark-boletim/template.boletim.php
@@ -50,7 +50,7 @@ get_header('boletim'); ?>
                     <div class="boletim-box" data-sr="enter bottom and move 50px wait .2s">
                         <?php
                         $thumb_id = get_post_thumbnail_id($_post->ID);
-                        $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+                        $thumb_url = wp_get_attachment_image_src($thumb_id,'destaque-boletim', true);
                         ?>
                         <div class="boletim-thumb" style="background-image:url('<?php echo $thumb_url[0]; ?>');" >
                         <a href="<?php echo get_permalink($_post->ID); ?>">
