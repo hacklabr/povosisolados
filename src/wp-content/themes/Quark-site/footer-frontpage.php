4a95d8c84b691d6f504ce819d62c86e62bceda90
Fernando Silva <liquuid@hacklab.com.br>
2016-01-21 20:04:50 -0200

    Adaptando o footer do boletim, mudando de 5 colunas para 3 colunas no footer.

--- src/wp-content/themes/Quark-site/footer-frontpage.php
+++ src/wp-content/themes/Quark-site/footer-frontpage.php
@@ -1,30 +1,36 @@
 <?php
 /**
- * The template for displaying the footer
+ * The template for displaying the footer from Boletim Page
  *
  */
+ global $post_original;
 
 ?>
-    	<?php do_action('quark_before_footer'); ?>
-    	<footer id="gk-footer" role="contentinfo">
-    		<div class="site">
-                <div id="gk-footer-nav">
-                    <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'footer-menu', 'fallback_cb' => false ) ); ?>
-                </div>
 
-        		<div id="gk-copyrights">
-                    <p class="copyright"><?php echo get_theme_mod('quark_copyright_text', 'WordPress Theme by <a href="https://www.gavick.com">GavickPro.com</a>'); ?></p>
+      <?php do_action('quark_before_footer'); ?>
+        <footer id="gk-footer" role="contentinfo">
+            <div class="site">
+                <div class="gk-cols" data-cols="3">
+                    <?php dynamic_sidebar('footer_1'); ?>
+                    <?php dynamic_sidebar('footer_2'); ?>
+                    <?php dynamic_sidebar('footer_3'); ?>
                 </div>
             </div>
-    	</footer><!-- end of #gk-footer -->
-    	<?php do_action('quark_after_footer'); ?>
+            <div class="site">
+                <div id="gk-copyrights">
+                    <p class="copyright"><?php _e('Desenvolvido por', 'quark'); ?><a href="http://hacklab.com.br/" class="hacklab"><span class="screen-reader-text">hacklab</a></a><?php _e('with','quark'); ?><a href="https://wordpress.org/">Wordpress</a></p>
+                </div>
+            </div>
+
+        </footer><!-- end of #gk-footer -->
+        <?php do_action('quark_after_footer'); ?>
     </div><!-- #gk-bg -->
-    
-    <?php if(get_theme_mod('quark_menu_classic', 0) == 0) : ?>
-	<?php do_action('quark_before_asidemenu'); ?>
-	<i id="close-menu">&times;</i>
-	<aside id="aside-menu">
+
+    <?php do_action('quark_before_asidemenu'); ?>
+    <i id="close-menu">&times;</i>
+    <aside id="aside-menu">
         <div>
+
             <?php if (is_active_sidebar('menu_top')) : ?>
             <?php do_action('quark_before_menu_top'); ?>
             <div id="gk-menu-top">
@@ -32,12 +38,104 @@
                     <?php dynamic_sidebar('menu_top'); ?>
                 </div>
             </div>
+
             <?php do_action('quark_after_menu_top'); ?>
             <?php endif; ?>
 
+            <?php get_search_form(); ?>
+            <?php  if ( $post->post_type == "page"): ?>
+              <h2 class="aside-title-boletim"><?php the_title(); ?></h2>
+            <?php else: ?>
+            	  <h2 class="aside-title-boletim"></h2>
+            <?php endif; ?>
+            <div class="language">
+                <ul>
+                  <?php pll_the_languages(array('dropdown' => 0, 'show_flags'=>1,'show_names'=>0));?>
+                </ul>
+            </div>
+
+            <?php  if ( $post->post_type == "post"): ?>
+
+              <h2 class="aside-title-boletim"><?php   ?></h2>
+            <?php endif; ?>
+            <h2 class="aside-title"><?php _e('Nesta edição', 'quark'); ?></h2>
+
+            <?php
+            the_post();
+            if ( $post->post_type == "page") {
+                $category = $post_original->post_name;
+            }
+            if ( $post->post_type == "post") {
+                $category = get_post_meta($post->ID, 'boletim', true);
+            }
+            $args_global = array(
+              'post_type' => 'post',
+              'order' => 'DESC',
+              'meta_key' => 'boletim',
+              'meta_value' => $category,
+              'posts_per_page' => -1
+            );
+
+            $loop_news = get_posts($args_global);
+
+            wp_reset_query(); ?>
+
+
             <nav id="aside-navigation" class="main-navigation" role="navigation">
-                <?php wp_nav_menu( array( 'theme_location' => 'mainmenu', 'menu_class' => 'nav-aside-menu', 'fallback_cb' => false ) ); ?>
+              <div class="menu-menu-boletim-container">
+              <!-- <?php wp_nav_menu( array( 'theme_location' => 'boletimmenu', 'menu_class' => 'nav-aside-menu', 'fallback_cb' => false ) ); ?> -->
+              <ul class="articles">
+                <?php foreach ( $loop_news as $_post ){    ?>
+
+                <li><a href="<?php echo get_permalink($_post->ID); ?>"><?php echo $_post->post_title; ?></a></li>
+                <?php }; wp_reset_query();?>
+              </ul>
+              </div>
+
             </nav><!-- #aside-navigation -->
+
+            <h2 class="aside-title"><?php _e('Boletins Anteriores', 'quark'); ?></h2>
+
+            <?php
+            $args_global = array(
+              'post_type' => 'page',
+              'order' => 'DESC',
+              'meta_query' => array(
+                                array(
+                                  'key' => 'is_boletim',
+                                  'value' => 'boletim',
+                                  'compare' => 'IN',
+                                )
+                              ),
+              'posts_per_page' => 10
+            );
+            $loop_boletim = new WP_Query( $args_global );
+            wp_reset_query();?>
+
+
+            <nav id="aside-navigation" class="main-navigation" role="navigation">
+              <div class="menu-menu-boletim-container">
+              <!-- <?php wp_nav_menu( array( 'theme_location' => 'boletimmenu', 'menu_class' => 'nav-aside-menu', 'fallback_cb' => false ) ); ?> -->
+              <ul class="articles">
+                <?php while ( $loop_boletim->have_posts()): $loop_boletim->the_post(); ?>
+                <li>
+                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </li>
+                <?php endwhile; wp_reset_query();?>
+              </ul>
+              </div>
+
+            </nav><!-- #aside-navigation -->
+
+            <?php
+                $archive_link = get_page_by_path('archive-boletins');
+                $archive_link = ($archive_link) ? get_the_permalink($archive_link->ID) : false;
+            ?>
+            <?php if ( $archive_link ) : ?>
+                <div class="entry-thumbnail-wrap">
+                    <a href="<?php echo $archive_link; ?>" class="btn">Veja mais boletins</a>
+                </div>
+            <?php endif;?>
 
             <?php if (is_active_sidebar('menu_bottom')) : ?>
             <?php do_action('quark_before_menu_bottom'); ?>
@@ -51,8 +149,7 @@
 
         </div>
     </aside><!-- #aside-menu -->
-	<?php do_action('quark_after_asidemenu'); ?>
-    <?php endif; ?>
+    <?php do_action('quark_after_asidemenu'); ?>
 
     <?php if(get_theme_mod('quark_login_popup', 1) == 1) : ?>
     <div id="gk-login-popup">
@@ -62,7 +159,7 @@
     <div id="gk-login-popup-overlay"></div>
     <?php endif; ?>
 
-	<!-- Root element of PhotoSwipe. Must have class pswp. -->
+    <!-- Root element of PhotoSwipe. Must have class pswp. -->
     <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="pswp__bg"></div>
         <div class="pswp__scroll-wrap">
@@ -75,9 +172,9 @@
             <div class="pswp__ui pswp__ui--hidden">
                 <div class="pswp__top-bar">
                     <div class="pswp__counter"></div>
-                    
+
                     <div class="pswp__preloader"></div>
-                    
+
                     <button class="pswp__button pswp__button--fs" title="<?php _e('Toggle fullscreen','quark'); ?>"></button>
                     <button class="pswp__button pswp__button--zoom" title="<?php _e('Zoom in/out','quark'); ?>"></button>
                     <button class="pswp__button pswp__button--share" title="<?php _e('Share','quark'); ?>"></button>
@@ -85,7 +182,7 @@
                 </div>
 
                 <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                    <div class="pswp__share-tooltip"></div> 
+                    <div class="pswp__share-tooltip"></div>
                 </div>
 
                 <button class="pswp__button pswp__button--arrow--left" title="<?php _e('Previous (arrow left)','quark'); ?>"></button>
@@ -97,7 +194,7 @@
             </div>
         </div>
     </div>
-    
+
     <?php wp_footer(); ?>
 </body>
 </html>
